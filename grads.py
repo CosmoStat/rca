f375092bb18090d6bdadc0adb8e293f87ac575ce
MorganSchmitz <morgan.schmitz@cea.fr>
2019-02-12 19:19:05 +0100

    Add weight update grad instance
    also move degration_op and its adjoint outside SourceGrad because we need it here too

--- grads.py
+++ grads.py
@@ -3,8 +3,115 @@ from modopt.opt.gradient import GradParent, GradBasic
 from modopt.math.matrix import PowerMethod
 import utils
 from scipy.signal import fftconvolve
+
+
+def degradation_op(S, A_i, shift_ker, D):
+    """ Shift and decimate reconstructed PSF."""
+    return utils.decim(fftconvolve(S.dot(A_i),shift_ker,mode='same'),
+                       D,av_en=0)
+
+def adjoint_degradation_op(x_i, shift_ker, D):
+    """ Apply adjoint of the degradation operator."""
+    return fftconvolve(utils.transpose_decim(x_i,D),
+                       shift_ker,mode='same')
         
+class CoeffGrad(GradParent, PowerMethod):
+    def __init__(self, data, S, VT, flux, sig, ker, ker_rot, D, data_type='float'):
+        self._grad_data_type = data_type
+        self.obs_data = data
+        self.op = self.MX 
+        self.trans_op = self.MtX 
+        self.S = np.copy(S)
+        self.VT = VT
+        self.flux = flux
+        self.sig = sig
+        self.ker = ker
+        self.ker_rot  = ker_rot
+        self.D = D
+        # save number of (superresolved) pixels to flatten stacks of images
+        self.hr_npix = np.prod(np.array(data.shape[:2])*D)
+        # initialize Power Method to compute spectral radius
+        PowerMethod.__init__(self, self.trans_op_op, 
+                        (S.shape[-1],VT.shape[0]), auto_run=False)
         
+        self._current_rec = None # stores latest application of self.MX
+
+    def update_S(self, new_S, update_spectral_radius=True):
+        self.S = new_S
+        if update_spectral_radius:
+            PowerMethod.get_spec_rad(self)
+
+    def MX(self, alph):
+        """Apply degradation operator and renormalize.
+
+        Parameters
+        ----------
+        S : np.ndarray
+            Input data array, a set of eigenPSFs
+
+        Returns
+        -------
+        np.ndarray result
+
+        """
+        normfacs = self.flux / (np.median(self.flux)*self.sig)
+        A = alph.dot(self.VT) 
+        dec_rec = np.array([nf * degradation_op(self.S,A_i,shift_ker) for nf,A_i,shift_ker 
+                       in zip(normfacs, A.T, utils.reg_format(self.ker))])
+        self._current_rec = utils.rca_format(dec_rec)
+        return self._current_rec
+
+    def MtX(self, x):
+        """MtX
+
+        This method calculates the action of the transpose of the matrix Mt on
+        the data X
+
+        Parameters
+        ----------
+        x : np.ndarray
+            Input data array, a cube of 2D images
+
+        Returns
+        -------
+        np.ndarray result
+
+        """
+        normfacs = self.flux / (np.median(self.flux)*self.sig)
+        x = utils.reg_format(x)
+        upsamp_x = np.array([nf * adjoint_degradation_op(x_i,shift_ker,self.D) for nf,x_i,shift_ker 
+                       in zip(normfacs, x, utils.reg_format(self.ker_rot))])
+        x, upsamp_x = utils.rca_format(x).reshape(), utils.rca_format(upsamp_x)
+        STx = self.S.reshape(-1,self.hr_npix).dot(x.reshape(self.hr_npix,-1))
+        return STx.dot(self.VT.T) #aka... "V"
+                
+    def cost(self, x, y=None, verbose=False):
+        """ Compute data fidelity term. ``y`` is unused (it's just so ``modopt.opt.algorithms.Condat`` can feed
+        the dual variable.)
+        """
+        if isinstance(self._current_rec, type(None)):
+            self._current_rec = self.MX(x)
+        cost_val = 0.5 * np.linalg.norm(self._current_rec - self.obs_data) ** 2
+        return cost_val
+                
+    def get_grad(self, x):
+        """Get the gradient step
+        This method calculates the gradient step from the input data
+        Parameters
+        ----------
+        x : np.ndarray
+            Input data array
+        Returns
+        -------
+        np.ndarray gradient value
+        Notes
+        -----
+        Calculates M^T (MX - Y)
+        """
+
+        self.grad = self.MtX(self.MX(x) - self.obs_data)
+      
+
 class SourceGrad(GradParent, PowerMethod):
     """Parameters
     ----------
@@ -51,16 +158,6 @@ class SourceGrad(GradParent, PowerMethod):
         if update_spectral_radius:
             PowerMethod.get_spec_rad(self)
 
-    def degradation_op(self, S, A_i, shift_ker):
-        """ Shift and decimate reconstructed PSF."""
-        return utils.decim(fftconvolve(S.dot(A_i),
-                           shift_ker,mode='same'),self.D,av_en=0)
-
-    def adjoint_degradation_op(self, x_i, shift_ker):
-        """ Apply adjoint of the degradation operator."""
-        return fftconvolve(utils.transpose_decim(x_i,self.D),
-                           shift_ker,mode='same')
-
     def MX(self, S):
         """Apply degradation operator and renormalize.
 
@@ -75,7 +172,7 @@ class SourceGrad(GradParent, PowerMethod):
 
         """
         normfacs = self.flux / (np.median(self.flux)*self.sig)
-        dec_rec = np.array([nf * self.degradation_op(S,A_i,shift_ker) for nf,A_i,shift_ker 
+        dec_rec = np.array([nf * degradation_op(S,A_i,shift_ker) for nf,A_i,shift_ker 
                        in zip(normfacs, self.A.T, utils.reg_format(self.ker))])
         self._current_rec = utils.rca_format(dec_rec)
         return self._current_rec
@@ -98,7 +195,7 @@ class SourceGrad(GradParent, PowerMethod):
         """
         normfacs = self.flux / (np.median(self.flux)*self.sig)
         x = utils.reg_format(x)
-        upsamp_x = np.array([nf * self.adjoint_degradation_op(x_i,shift_ker) for nf,x_i,shift_ker 
+        upsamp_x = np.array([nf * adjoint_degradation_op(x_i,shift_ker,self.D) for nf,x_i,shift_ker 
                        in zip(normfacs, x, utils.reg_format(self.ker_rot))])
         x, upsamp_x = utils.rca_format(x), utils.rca_format(upsamp_x)
         return upsamp_x.dot(self.A.T)
